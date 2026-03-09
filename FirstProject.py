86ac0ba23770aa01d871d1cd33e370b0b09486d9
Arabian_Aka <smayansecret1@gmail.com>
2026-03-09 18:53:14 +0530

    Add files via upload

--- FirstProject.py
+++ FirstProject.py
@@ -3,3 +3,13 @@ print("4")
 print("hello python \n")
 print("hello",5)
 print("welcome to",end='*')
+print("\n ...........")
+x=5
+y="John"
+
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+name=input("enter your name: ")
+print("\n Hello",name,"\n welcome to codingal")
