e0167e42a915f8e1b74d1b37b929e284ef32a9ed
Arabian_Aka <smayansecret1@gmail.com>
2026-03-10 17:54:35 +0530

    Add files via upload

---
+++ homeoworkstoringandprintingbirthdays.py
@@ -0,0 +1,39 @@
+
+print("Wraning- Enter the birthday in 'date/month/year' format⚠️")
+
+print("\n")
+
+Birthday_1_Name=input("Enter Name: ")
+Birthday_1=input("Enter Birthday🎂: ")
+
+print("\n")
+
+Birthday_2_Name=input("Enter Name: ")
+Birthday_2=input("Enter Birthday🎂: ")
+
+print("\n")
+
+Birthday_3_Name=input("Enter Name: ")
+Birthday_3=input("Enter Birthday🎂: ")
+
+print("\n")
+
+Birthday_4_Name=input("Enter Name: ")
+Birthday_4=input("Enter Birthday🎂: ")
+
+print("\n")
+
+Birthday_5_Name=input("Enter Name: ")
+Birthday_5=input("Enter Birthday🎂: ")
+
+print("\n",end="")
+
+print("Happy birthday",Birthday_1_Name,"🎉🎊")
+print("Happy birthday",Birthday_2_Name,"🎉🎊")
+print("Happy birthday",Birthday_3_Name,"🎉🎊")
+print("Happy birthday",Birthday_4_Name,"🎉🎊")
+print("Happy birthday",Birthday_5_Name,"🎉🎊")
+
+print("\n")
+
+print("Best wishes to you all✨🌟")
