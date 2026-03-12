ef12a196af1bcdb319ba8deee62a3b9e91ef894b
Arabian_Aka <smayansecret1@gmail.com>
2026-03-12 19:01:20 +0530

    Add files via upload

---
+++ inputformuser.py
@@ -0,0 +1,25 @@
+print("\n")
+
+a=5
+print(a)
+print("The type of a is:",type(a))
+
+print(" \n")
+
+b=2.5
+print(b)
+print("The type of b is:",type(b))
+
+print(" \n")
+
+c="coding"
+print(c)
+print("The type of c is:",type(c))
+
+print(" \n")
+
+d=True
+print(d)
+print("The type of d is:",type(d))
+
+print("\n")
---
+++ typecasting.py
@@ -0,0 +1,93 @@
+from rich import print
+
+print("\n")
+ 
+
+
+name=input("Enter your name :) :")
+
+print("\n")
+
+age=int(input("Enter your age :) :"))
+
+print("\n")
+
+identity=bool(input("Are you a student (answer in True or False (use Capital T/R)) :) :"))
+
+print("\n")
+
+weight=float()
+
+print("\n")
+
+print(name)
+
+print("\n")
+
+print("Date type of name is:", type(name))
+
+print("\n")
+
+print("\n")
+
+print(age)
+
+print("\n")
+
+print("data type of age is:",type(age))
+
+print("\n")
+
+print("\n")
+
+print("Are you a student:",identity)
+
+print("\n")
+
+print("data type of your answer 'Are you a student' is",type(identity))
+
+print("\n")
+
+print("\n")
+
+print("Weight:",weight)
+
+print("\n")
+
+print("data type of weight is:",type(weight))
+
+print("\n")
+
+print("\n")
+
+print("\n after type casting......")
+
+print("\n")
+
+age = str(age)
+
+print("\n")
+
+print(age)
+
+print("\n")
+
+print("Data Type of age is", type(age))
+
+print("\n")
+
+print("\n")
+
+weight = int(weight)
+
+print("\n")
+
+print(weight)
+
+print("\n")
+
+print("Data Type of Weight is", type(weight))
+
+print("\n")
+
+print("\n")
