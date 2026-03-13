b61578771fcb58f3ce8507851ba3956f64f1f8e3
Arabian_Aka <smayansecret1@gmail.com>
2026-03-13 18:09:17 +0530

    Add files via upload

---
+++ homeworkcongratulationmessage.py
@@ -0,0 +1,17 @@
+print("\n")
+
+print("\n")
+
+name=input("Enter your name :) :")
+
+print("\n")
+
+achivement=input("Enter your latest achivement (dont use any pronouns) :) :")
+
+print("\n")
+
+print("Congratulations to you",name,"for",achivement)
+
+print("\n")
+
+print("\n")
