1ddefad461318f9a8112e829881a38c384739d4f
Arabian_Aka <smayansecret1@gmail.com>
2026-03-16 19:00:23 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,33 @@
+print("\n")
+
+print("\n")
+
+print("Hi, Welome to the average app")
+
+print("\n")
+
+print("Find out the average of anything")
+
+print("\n")
+
+item=input("Enter the thing of which we need to take out the average of (Make sure that the item is not more then 3 or less then 3) :) :")
+
+print("\n")
+
+_1=int(input(f"Enter the first value of {item}"))
+
+print("\n")
+
+_2=int(input(f"Enter the second value of {item}"))
+
+print("\n")
+
+_3=int(input(f"Enter the second value of {item}"))
+
+print("\n")
+
+average=(_1+_2+_3)/3
+
+print("\n")
+
+print("so the average of",item,"is",average)
---
+++ denomination.py
@@ -0,0 +1,31 @@
+print("\n")
+
+print("\n")
+
+print("welcome to the notes finder app")
+
+print("we will tell the no of notes you have")
+
+print("To get started ")
+
+print("welcome to the notes finder app")
+
+print("welcome to the notes finder app")
+
+amount=input("Enter the amount")
+
+note1000= amount//1000
+
+amount=amount%1000
+
+note500= amount//500
+
+amount=amount%500
+
+note100=amount//100
+
+print("So after deep thing you have-")
+
+print("\n")
+
+print("")
