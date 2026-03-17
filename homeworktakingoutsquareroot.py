0555423c5fb80bb47f264ca3764f314847d1aea0
Arabian_Aka <smayansecret1@gmail.com>
2026-03-17 10:40:43 +0530

    Add files via upload

---
+++ homeworktakingoutsquareroot.py
@@ -0,0 +1,19 @@
+import math
+
+print("\n")
+
+print("Welcome to the 'Square Root Finder' app ")
+
+print("\n")
+
+num=int(input("Enter a number and we will tell it's square root :) :"))
+
+print("\n")
+
+square_root=math.sqrt(num)
+
+print("after deep thinking",num,"'s square root is :",square_root )
+
+print("\n")
+
+print("\n")
