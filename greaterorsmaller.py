0900c68cf2bf654772e62a987a7a0a12f28502a9
Arabian_Aka <smayansecret1@gmail.com>
2026-03-19 18:34:42 +0530

    Add files via upload

---
+++ greaterorsmaller.py
@@ -0,0 +1,25 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the greater or smaller app, us the number and we will tell smaller or greater")
+
+print("\n")
+
+num=int(input("Enter the main number :) :"))
+
+print("\n")
+
+compared=int(input("Enter the number to which we are comparing to :) :"))
+
+print("\n")
+
+if (num>compared):
+    print(num,"is greater then",compared)
+
+else:
+    print(num,"is smaller then",compared)
+
+print("\n")
+
+print("\n")
