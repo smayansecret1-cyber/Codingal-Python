9cfc1dc4ac9824953f1f11d4cd156c92b3e527f1
Arabian_Aka <smayansecret1@gmail.com>
2026-03-19 18:41:55 +0530

    Add files via upload

---
+++ oddoreven.py
@@ -0,0 +1,21 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the Odd Or Even app enter a number and we will tell wether it's odd or even")
+
+print("\n")
+
+num=int(input("Enter a number to be checked :) :"))
+
+print("\n")
+
+print("The number we are checking is ",num)
+
+print("\n")
+
+if num%2==0:
+    print(num,"is even")
+
+else:
+    print(num,"is odd")
