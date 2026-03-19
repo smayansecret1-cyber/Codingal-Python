e9614857930288eaaba8c0b34b5186641d33d31d
Arabian_Aka <smayansecret1@gmail.com>
2026-03-19 18:15:37 +0530

    Add files via upload

---
+++ ConditionalStatements.py
@@ -0,0 +1,19 @@
+print("\n")
+
+print("\n")
+
+num=int(input("Enter a positive number :) :"))
+
+print("\n")
+
+if num >0:
+    print(num,"is a positive number")
+
+print("\n")
+
+_num=int(input("Enter a negative number number :) :"))
+
+print("\n")
+
+if _num >0:
+    print(_num,"is a negative number")
