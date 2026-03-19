c5eca89d436651adb1ec72b8421ac36e4c8f69e7
Arabian_Aka <smayansecret1@gmail.com>
2026-03-19 18:27:14 +0530

    Add files via upload

---
+++ profitorloss.py
@@ -0,0 +1,27 @@
+print("\n")
+
+print("\n")
+
+print("Welocome to the profit or loss app enter the cp and sp and we will tell u its profit or loss")
+
+print("\n")
+
+CP=float(input("Enter The Cost price :) :"))
+
+print("\n")
+
+SP=float(input("Enter The Selling Prince :) :"))
+
+print("\n")
+
+if (CP<SP):
+    amt=SP-CP
+    print("Mate u got a profit of",amt)
+
+else:
+    amt=CP-SP
+    print("Mate you got a loss of",amt)
+
+print("\n")
+
+print("\n")
