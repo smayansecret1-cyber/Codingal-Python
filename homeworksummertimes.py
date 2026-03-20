db23d8e12287c5531a512813f3b1ece2f68b3049
Arabian_Aka <smayansecret1@gmail.com>
2026-03-20 13:37:48 +0530

    Add files via upload

---
+++ homeworksummertimes.py
@@ -0,0 +1,50 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the season app")
+
+print("\n")
+
+print("Enter the season and we will tell what kinds of clothes and foods/drinks you should eat/drink/wear")
+
+print("\n")
+
+season=input("Chose a season Summer/Winter/Spring/Monsoon (Use the exact form and formating while chosing the season) :) :")
+
+print("\n")
+
+if season=="Summer":
+    print("In Summers you should wear clothes which are \n \n 1.Light, loose clothes \n \n 2.Light colors ")
+    
+    print("\n")
+
+    print("In Summers you should eat/drink \n \n 1.Hydrating foods: watermelon, cucumber, oranges \n \n 2.Drink lots of water, lemonade, coconut water ")
+
+elif season=="Winter":
+    print("IN winters you should wear \n \n 1.Warm layers \n \n 2.Woolen clothes \n \n 3.Gloves, scarf, socks")
+
+    print("\n")
+
+    print("In winters you should eat/drink:\n \n 1.Hot foods: soups, tea, coffee \n \n 2.Energy-rich foods: nuts, dry fruits \n \n 3.Ghee, butter (in moderate amount)")
+
+elif season=="Spring":
+    print("In spring you should wear \n \n 1.Comfortable, light layers \n \n 2.Not too thick, not too thin \n \n 3.Light jackets or full sleeves (weather changes)")
+
+    print("\n")
+
+    print("In spring you should eat/drink \n \n 1.Fresh fruits & veggies \n \n 2.Balanced diet \n \n 3.Light and healthy meals")
+
+elif season=="Monsoon":
+    print("In monsoon you should wear \n \n 1.Light clothes that dry fast (polyester, nylon) \n \n 2.Avoid heavy jeans \n \n 3.Raincoat, umbrella")
+
+    print("\n")
+
+    print("In monsoon you should eat/drink \n \n 1.Fresh, hot, home-cooked food \n \n 2.Tea, soups \n \n 3.Boiled or cooked vegetables")
+
+else :
+    print("Season not found pls check the formating of the season and also check the spelling")
+
+print("\n")
+
+print("\n")
