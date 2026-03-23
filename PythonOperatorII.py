59d2f947c1991ebf290670aa01d238fb614b7708
Arabian_Aka <smayansecret1@gmail.com>
2026-03-23 18:34:13 +0530

    Add files via upload

---
+++ PythonOperatorII.py
@@ -0,0 +1,25 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the operator thingy")
+
+print("\n")
+
+a=int(input("Enter a number :) :"))
+
+print("\n")
+
+b=int(input("Enter another number :) :"))
+
+print("\n")
+
+if a>0 and b>0:
+    print("The number is greater then 0")
+
+else:
+    print("The number is less then 0")
+
+print("\n")
+
+print("\n")
---
+++ exrtra1.py
@@ -0,0 +1,24 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the greater then or less then app, Enter a number and we will tell you if its greater then 16")
+
+print("\n")
+
+num=int(input("Enter a number to be checked :) :"))
+
+print("\n")
+
+if num>16:
+    print(num,"is greater then 16")
+
+elif num==16:
+    print(num,"is equal to 16")
+
+else :
+    print(num,"is less then 16")
+
+print("\n")
+
+print("\n")
