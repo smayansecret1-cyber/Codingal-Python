7bdd88a43bfc34dc761d77dbf2198dd1e0d6ed1f
Arabian_Aka <smayansecret1@gmail.com>
2026-03-24 16:40:49 +0530

    Add files via upload

---
+++ homeworkcheckingalphabets.py
@@ -0,0 +1,23 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the 'letter or not' app,enter a letter charecter emoji or anything and we will tell is it a letter or not ")
+
+print("\n")
+
+userinput=input("Enter anything (Enter only a single letter emoji or smn):) :")
+
+print("\n")
+
+if userinput == "a" or userinput == "b" or userinput == "c" or userinput == "d" or userinput == "e" or userinput == "f" or userinput == "g" or userinput == "h" or userinput == "i" or userinput == "j" or userinput == "k" or userinput == "l" or userinput == "m" or userinput == "n" or userinput == "o" or userinput == "p" or userinput == "q" or userinput == "r" or userinput == "s" or userinput == "t" or userinput == "u" or userinput == "v" or userinput == "w" or userinput == "x" or userinput == "y" or userinput == "z" or userinput == "A" or userinput == "B" or userinput == "C" or userinput == "D" or userinput == "E" or userinput == "F" or userinput == "G" or userinput == "H" or userinput == "I" or userinput == "J" or userinput == "K" or userinput == "L" or userinput == "M" or userinput == "N" or userinput == "O" or userinput == "P" or userinput == "Q" or userinput == "R" or userinput == "S" or userinput == "T" or userinput == "U" or userinput == "V" or userinput == "W" or userinput == "X" or userinput == "Y" or userinput == "Z":
+
+    print(userinput,"is(has) a letter")
+
+else :
+
+    print(userinput,"isn't(does not have) a letter")
+
+print("\n")
+
+print("\n")
