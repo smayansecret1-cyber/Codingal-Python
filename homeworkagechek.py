3634cf1cd2671d566e10f732676c2ba8f9a70285
Arabian_Aka <smayansecret1@gmail.com>
2026-03-27 12:02:33 +0530

    Add files via upload

---
+++ homeworkagechek.py
@@ -0,0 +1,33 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the enrollment app, answer the following questions and we will tell if you can enroll into this class")
+
+print("\n")
+
+name=input("Enter you name :) :")
+
+print("\n")
+
+age=int(input(f"Enter your age {name} :) :"))
+
+print("\n")
+
+if age<20:
+
+    if age>10:
+
+        print(f"Congrats {name}, you can enroll in this class")
+
+    elif age<10:
+
+        print(f"Sorry {name}, you can't enroll in this class")
+
+else:
+
+    print(f"Sorry {name},you can't enroll in this class")
+
+print("\n")
+
+print("\n")
