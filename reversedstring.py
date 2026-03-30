096c15380a9c179ab1aa4cb4bb6cd8636e8232ec
Arabian_Aka <smayansecret1@gmail.com>
2026-03-30 18:59:16 +0530

    Add files via upload

---
+++ reversedstring.py
@@ -0,0 +1,27 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the reversed word app,enter anything and we will reverse it")
+
+print("\n")
+
+string=input("Enter the word sentence or anything you want reversed :) :")
+
+string_=""
+
+print("\n")
+
+for  i in string:
+
+    string_=i+string_
+
+print(f"you wrote {string}")
+
+print("\n")
+
+print(f"REversed {string_}")
+
+print("\n")
+
+print("\n")
