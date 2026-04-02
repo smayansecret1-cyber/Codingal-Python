9ef9273ef3b28e7a497613458ecd9b187c5f8768
Arabian_Aka <smayansecret1@gmail.com>
2026-04-02 19:04:48 +0530

    Add files via upload

---
+++ amstrong.py
@@ -0,0 +1,19 @@
+print("\n")
+
+print("\n")
+
+num=int(input("enter a random number :) :"))
+
+print("\n")
+
+while (num>0):
+
+    a=num%10
+
+    print(a,"\n")
+
+    num=num//10
+
+print("\n")
+
+print("\n")
