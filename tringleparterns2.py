4e9f5fe35d274a9b7ded9565d1e9d043c02581e1
Arabian_Aka <smayansecret1@gmail.com>
2026-04-09 18:58:03 +0530

    Add files via upload

---
+++ tringleparterns2.py
@@ -0,0 +1,25 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the tringle partern app, enter he range and we will tell the tringle partern")
+
+print("\n")
+
+ll=int(input("Enrer the number of lines :) :"))
+
+print("\n")
+
+num=0
+
+for i in range(1,ll):
+
+    for j in range (1,i+1):
+
+        num=num+1
+
+        print(num,end="\t")
+
+    print("\n")
+
+print("\n")
