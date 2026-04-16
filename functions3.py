33c70721d9ce2e3a9860bf8c06d2b56f15e41e80
Arabian_Aka <smayansecret1@gmail.com>
2026-04-16 19:00:42 +0530

    Add files via upload

---
+++ functions3.py
@@ -0,0 +1,19 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the calculator, enter the numbers and chose the operation")
+
+print("\n")
+
+num1=int(input("Enter the first number :) :"))
+
+print("\n")
+
+num2=int(input("Enter second number :) :"))
+
+print("\n")
+
+operation=input("Chose the operation \n \n a.Additon \n \n b.Subsrtact \n \n c.Multiply \n \n d.Divide \n \n Enter the operation :) :")
+
+print("\n")
