e9d5b13a5fd13b574dd306539c2046b4972ab6e7
Arabian_Aka <smayansecret1@gmail.com>
2026-04-21 18:10:16 +0530

    Add files via upload

---
+++ homeworkcircumfranceofacircle.py
@@ -0,0 +1,23 @@
+print("\n")
+
+print("\n")
+
+print("Wrlcome to the cirle measuremnt app, enter the details and we will tell the circumfrance")
+
+print("\n")
+
+radius=float(input("Enter the radius of the circle :) :"))
+
+print("\n")
+
+def circumfrance(a):
+
+    return(2 * 3.14 * a)
+
+circum= circumfrance(radius)
+
+print(f"The circumfrance of the cirlce is {circum}")
+
+print("\n")
+
+print("\n")
