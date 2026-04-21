f53b100a2431a291386a9943a2a06eab362979e3
Arabian_Aka <smayansecret1@gmail.com>
2026-04-21 18:16:57 +0530

    Add files via upload

---
+++ homeworkshutdown.py
@@ -0,0 +1,31 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the shut down app, enter the command and we will shut down ")
+
+print("\n")
+
+command=input("Do you want to shut down? (yes/no) :) : ")
+
+print("\n")
+
+
+def shutdown(a):
+    if a == "yes":
+
+        print("Shutting down :)")
+
+    elif a == "no":
+
+        print("Shutdown aborted :)")
+
+    else:
+
+        print("Invalid input :|")
+
+shutdown1= shutdown(command)
+
+print("\n")
+
+print("\n")
