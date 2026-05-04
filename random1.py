49914e1a69396e4574f5d9ceca39a2fa2a524b18
Arabian_Aka <smayansecret1@gmail.com>
2026-05-04 18:32:01 +0530

    Add files via upload

---
+++ random1.py
@@ -0,0 +1,27 @@
+import random
+
+print("\n")
+
+print("\n")
+
+print("WElcome to the random app,enter the range and we will give a random number")
+
+print("\n")
+
+range1=int(input("Enter the starting number :) :"))
+
+print("\n")
+
+range2=int(input("Enter the ending number :) :"))
+
+print("\n")
+
+def bru(a,b):
+
+    print(random.randint(a,b))
+
+bru(range1,range2)
+
+print("\n")
+
+print("\n")
