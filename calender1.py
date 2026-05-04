e1e855e026cf55d324a618c1cb8ab23e0cc4279d
Arabian_Aka <smayansecret1@gmail.com>
2026-05-04 19:05:10 +0530

    Add files via upload

---
+++ calender1.py
@@ -0,0 +1,21 @@
+import calendar
+
+print("\n")
+
+print("\n")
+
+year = 2026
+
+month = 1
+
+for i in range (1,13):
+
+    print(calendar.month(year, month),end="")
+
+    print("\n")
+
+    month=month+1
+
+print("\n")
+
+print("\n")
