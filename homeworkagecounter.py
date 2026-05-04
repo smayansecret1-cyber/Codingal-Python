b4cf02565c1bf9dca5b3b06b2f0ff5d5e430cd2b
Arabian_Aka <smayansecret1@gmail.com>
2026-05-04 18:19:22 +0530

    Add files via upload

---
+++ homeworkagecounter.py
@@ -0,0 +1,31 @@
+print("\n")
+
+print("\n")
+
+print("\nWelcome to the Age Checker App\n")
+
+print("\n")
+
+age = int(input("Enter your age :) : "))
+
+print("\n")
+
+if age <= 18:
+
+    print("Invalid age")
+
+else:
+
+    print("Valid age")
+    
+    if age % 2 == 0:
+
+        print("Age is EVEN")
+
+    else:
+
+        print("Age is ODD")
+
+print("\n")
+
+print("\n")
