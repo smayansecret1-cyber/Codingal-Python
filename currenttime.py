83d93f6f5fe4763d9e1c0520895cba21a681409f
Arabian_Aka <smayansecret1@gmail.com>
2026-05-04 18:38:26 +0530

    Add files via upload

---
+++ currenttime.py
@@ -0,0 +1,15 @@
+import datetime
+
+print("\n")
+
+print("\n")
+
+print("Welcome to the time app,here is the current time according to your time zone")
+
+print("\n")
+
+print(datetime.datetime.now())
+
+print("\n")
+
+print("\n")
