4477194737ad7c88c00a0c418b1177f26ebfc622
Arabian_Aka <smayansecret1@gmail.com>
2026-05-07 17:58:48 +0530

    Add files via upload

---
+++ homeworktansincos.py
@@ -0,0 +1,35 @@
+import math
+
+print("\n")
+
+print("\n")
+
+print("WElcome to the sin cos and tan app, enter the angle and we will tell the value")
+
+print("\n")
+
+angle = float(input("Enter angle in degrees :) : "))
+
+print("\n")
+
+radian = math.radians(angle)
+
+sin_value = math.sin(radian)
+
+cos_value = math.cos(radian)
+
+tan_value = math.tan(radian)
+
+print("\nSin =", sin_value)
+
+print("\n")
+
+print("Cos =", cos_value)
+
+print("\n")
+
+print("Tan =", tan_value)
+
+print("\n")
+
+print("\n")
