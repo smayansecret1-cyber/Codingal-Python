c686750e43b7c73b8b2a82d1df8381e33345f72b
Arabian_Aka <smayansecret1@gmail.com>
2026-05-10 12:07:58 +0530

    Add files via upload

---
+++ homeworkmonthnames.py
@@ -0,0 +1,21 @@
+import calendar
+
+print("\n")
+
+print("\n")
+
+print("Welcome to the month app,we will tell you the names of each month")
+
+print("\n")
+
+month=1
+
+while (month<13):
+
+    print(calendar.month_name[month],"\n")
+
+    month = month + 1
+
+print("\n")
+
+print("\n")
