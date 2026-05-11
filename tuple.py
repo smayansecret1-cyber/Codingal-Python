2ab6a2cf3d9c5f9623d89966309ebc5788b48c0f
Arabian_Aka <smayansecret1@gmail.com>
2026-05-11 18:54:24 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,75 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the tuple museum, here we will showcase alo forms of tupple")
+
+print("\n")
+
+print("\n")
+
+tuple1=(1,2.3,False,"hi")
+
+print("A tuple with all forms of data types")
+
+print("\n")
+
+print(tuple1)
+
+print("\n")
+
+print("\n")
+
+tuplex=(1,2,3,4,5,6,7,8,)
+
+tuplex1=tuplex
+
+print("adding a value to tuple")
+
+print("\n")
+
+print("before" ,tuplex,"after",tuplex1 +(9,))
+
+print("\n")
+
+print("\n")
+
+tuple2=(50,60,70,80,90,50)
+
+print("Finding no of elements in a tupple")
+
+print("\n")
+
+print(tuple2)
+
+print("\n")
+
+print("Number of 50 in tupple is",tuple2.count(50))
+
+print("\n")
+
+print("\n")
+
+print("Scling tecnique")
+
+print("\n")
+
+tuplex3 = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+
+print(tuplex3)
+
+print("\n")
+
+_slice = tuplex3[3:5]
+
+print(_slice)
+
+print("\n")
+
+_slice = tuplex3[:6]
+
+print(_slice)
+
+print("\n")
+
+print("\n")
