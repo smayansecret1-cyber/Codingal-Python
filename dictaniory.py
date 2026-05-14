aba52c5c51c23c8b176444fc552391cdbc344d73
Arabian_Aka <smayansecret1@gmail.com>
2026-05-14 18:36:39 +0530

    Add files via upload

---
+++ dictaniory.py
@@ -0,0 +1,17 @@
+print("\n")
+
+print("\n")
+
+print("WElcome to the dictaniory app,we will tell the key and value of the dictaniory")
+
+print("\n")
+
+words={"Name":"Smayan","Grade":"7","Section":"I"}
+
+for key, value in words.items():
+
+    print(f"{key} is {value} \n")
+
+print("\n")
+
+print("\n")
