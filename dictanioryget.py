184295c12e3a240819975bb8eca3963127211e8a
Arabian_Aka <smayansecret1@gmail.com>
2026-05-14 18:51:22 +0530

    Add files via upload

---
+++ dictanioryget.py
@@ -0,0 +1,27 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the country code app,we will tell the country code of a country")
+
+print("\n")
+
+country_code = {'India': '91','Australia': '61','Nepal': '977'}
+
+print("Country code for India -")
+
+print("\n")
+
+print(country_code.get('India', 'Not Found'))
+
+print("\n")
+
+print("Country code for Japan -")
+
+print("\n")
+
+print(country_code.get('Japan', 'Not Found'))
+
+print("\n")
+
+print("\n")
