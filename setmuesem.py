839335a3a710865a7c6aa5c942ddb71edef033ed
Arabian_Aka <smayansecret1@gmail.com>
2026-05-18 18:59:18 +0530

    Add files via upload

---
+++ setmuesem.py
@@ -0,0 +1,41 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the set muesem,enjoy")
+
+print("\n")
+
+set1={1,2,3,4}
+
+print(f"set1 is {set1}")
+
+print("\n")
+
+set2={3,4,5,6}
+
+print(f"set2 is {set2}")
+
+print("\n")
+
+print(set1.intersection(set2))
+
+print("\n")
+
+print(set1.union(set2))
+
+print("\n")
+
+print(set1.symmetric_difference(set2))
+
+print("\n")
+
+print(set1.difference(set2))
+
+print("\n")
+
+print(set2.difference(set1))
+
+print("\n")
+
+print("\n")
