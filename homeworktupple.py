1a4877fecca59cfebddf8780ed37532c75eb5061
Arabian_Aka <smayansecret1@gmail.com>
2026-05-18 17:16:46 +0530

    Add files via upload

---
+++ homeworktupple.py
@@ -0,0 +1,21 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the tuple multiplaier app,we will multiply the tupple")
+
+print("\n")
+
+tupple1=(1,2,3,4,5,6,7,8,9,10)
+
+print(f"This is the tupple : {tupple1}")
+
+print("\n")
+
+multi=tupple1[0]*tupple1[1]*tupple1[2]*tupple1[3]*tupple1[4]*tupple1[5]*tupple1[6]*tupple1[7]*tupple1[8]*tupple1[9]
+
+print(f"The answer of the tupple is :{multi}")
+
+print("\n")
+
+print("\n")
