b7cd8d9e1bc6c0495c2324d820a7f1f2d455a7cf
Arabian_Aka <smayansecret1@gmail.com>
2026-05-25 14:32:52 +0530

    Add files via upload

---
+++ homeworksymetricdifrence.py
@@ -0,0 +1,25 @@
+print("\n")
+
+print("\n")
+
+print("WElcome to the set app,we will tell the set symetric difrence")
+
+print("\n")
+
+set1={1,2,3,4}
+
+print(f"set1 is {set1}")
+
+print("\n")
+
+set2={3,4,5,6}
+
+print(f"set2 is {set2}")
+
+print("\n")
+
+print(set1.symmetric_difference(set2))
+
+print("\n")
+
+print("\n")
