7fc3ee646a6e242cd871ae2b4a7370988441ef4a
Arabian_Aka <smayansecret1@gmail.com>
2026-05-28 18:55:16 +0530

    Add files via upload

---
+++ classandobj2.py
@@ -0,0 +1,27 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the class and obj app,we will tell the max speed,milage of the car")
+
+print("\n")
+
+class vehicle:
+
+    def __init__(self, max_speed, mileage):
+
+        self.max_speed = max_speed
+
+        self.mileage = mileage
+
+modeLX = vehicle(240, 18)
+
+print("Model Max Speed:", modeLX.max_speed)
+
+print("\n")
+
+print("Model Mileage:", modeLX.mileage)
+
+print("\n")
+
+print("\n")
