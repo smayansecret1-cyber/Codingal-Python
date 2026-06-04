42d3b7f5c94a46439271c864478e4c25f80cd156
Arabian_Aka <smayansecret1@gmail.com>
2026-06-04 18:40:57 +0530

    Add files via upload

---
+++ inherantence.py
@@ -0,0 +1,37 @@
+print("\n")
+
+print("\n")
+
+print("Welcome to the bus app,we will tell about buses ")
+
+print("\n")
+
+class Car:
+
+    def __init__(self,name,speed,maliage):
+
+        self.name= name
+
+        self.speed= speed
+
+        self.maliage= maliage
+
+class Bus(Car):
+
+   pass
+
+a = Bus("Volvo","120","15")
+
+print(a.name)
+
+print("\n")
+
+print(a.speed)
+
+print("\n")
+
+print(a.maliage)
+
+print("\n")
+
+print("\n")
