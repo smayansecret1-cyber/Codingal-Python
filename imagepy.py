fad2e020b268b79fe6d4902f5586c46b54644983
Arabian_Aka <smayansecret1@gmail.com>
2026-06-18 19:01:15 +0530

    Add files via upload

---
+++ imagepy.py
@@ -0,0 +1,72 @@
+import pygame
+
+import sys
+
+pygame.init()
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+
+display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption('Adding image and background image')
+
+try:
+   background_image = pygame.transform.scale(
+      
+       pygame.image.load('bruh.jpg').convert(),
+
+       (SCREEN_WIDTH, SCREEN_HEIGHT))
+  
+   penguin_image = pygame.transform.scale(
+      
+       pygame.image.load('ahem.jpg').convert_alpha(),
+
+       (200, 200))
+except FileNotFoundError as e:
+   
+   print(f"Error loading image: {e}")
+
+   pygame.quit()
+
+   sys.exit()
+
+penguin_rect = penguin_image.get_rect(center=(SCREEN_WIDTH // 2,
+                                           SCREEN_HEIGHT // 2 - 30))
+
+font = pygame.font.Font(None, 36) 
+
+text = font.render('Hello Pygame!', True, pygame.Color('black'))
+
+text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 110))
+
+def game_loop():
+   
+   clock = pygame.time.Clock()
+
+   running = True
+  
+   while running:
+       
+       for event in pygame.event.get():
+           
+           if event.type == pygame.QUIT:
+               
+               running = False
+      
+       display_surface.blit(background_image, (0, 0))  
+
+       display_surface.blit(penguin_image, penguin_rect) 
+
+       display_surface.blit(text, text_rect)  
+      
+       pygame.display.flip()  
+
+       clock.tick(30) 
+  
+   pygame.quit()
+
+   sys.exit()  
+
+if __name__ == '__main__':
+   
+   game_loop()
